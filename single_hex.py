7e61d384c7f1fc3b9087102b12cb130c769b1bbf
imt-salt <caleb.salt@imtapps.com>
2018-05-30 22:36:57 -0400

    get verticies for single hex

---
+++ single_hex.py
@@ -0,0 +1,66 @@
+from collections import namedtuple
+from decimal import Decimal
+import math
+
+
+Point = namedtuple('Point', ['x', 'y'])
+
+
+def hex_corner(center, size, i):
+    angle_deg = 60 * i
+    angle_rad = math.pi / 180 * angle_deg
+    print('<>' * 10)
+    print(i)
+    print(angle_deg)
+    print(angle_rad)
+    print(math.cos(angle_rad))
+    print(math.sin(angle_rad))
+    return Point(center.x + size * math.cos(angle_rad),
+                 center.y + size * math.sin(angle_rad))
+
+
+center = Point(0, 0)
+size = 5
+for side in range(6):
+    corner = hex_corner(center, size, side)
+    print(f'Corner {side}: {corner}')
+
+
+#  ====================================================================================================
+print('<<<<>>>>'*10)
+
+sin = {
+    0: Decimal('0'),
+    60: Decimal('0.866025403784439'),
+    120: Decimal('0.866025403784439'),
+    180: Decimal('0'),
+    240: Decimal('-0.866025403784439'),
+    300: Decimal('-0.866025403784439'),
+}
+
+cos = {
+    0: Decimal('1'),
+    60: Decimal('0.5'),
+    120: Decimal('-0.5'),
+    180: Decimal('-1'),
+    240: Decimal('-0.5'),
+    300: Decimal('0.5'),
+}
+
+
+def enum_hex_corner(center, size, i):
+    angle_deg = 60 * i
+    print('<>' * 10)
+    print(i)
+    print(angle_deg)
+    print(sin[angle_deg])
+    print(cos[angle_deg])
+    return Point(center.x + size * cos[angle_deg],
+                 center.y + size * sin[angle_deg])
+
+
+center = Point(0, 0)
+size = 5
+for side in range(6):
+    corner = enum_hex_corner(center, size, side)
+    print(f'Corner {side}: {corner}')
